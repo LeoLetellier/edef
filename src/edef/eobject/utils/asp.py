58c7fa3b04fb9fd2f8db9e45692ef13c57f02cd2
LeoLetellier <leo.letellier.6@gmail.com>
2025-03-12 18:31:38 +0100

    add asp functions to wrapper

--- src/edef/eobject/utils/asp.py
+++ src/edef/eobject/utils/asp.py
@@ -130,3 +130,101 @@ def pc_align(
             output_prefix,
         )
     )
+
+
+@validate_call
+def pc_merge(
+    pc_inputs: Union[List[str], str],
+    pc_output: str,
+    **opts,
+):
+    pc_inputs_str = arg_to_str(pc_inputs)
+    opts_str = arg_to_str(opts)
+
+    run(
+        "pc_merge {} {} -o {}".format(
+            opts_str,
+            pc_inputs_str,
+            pc_output,
+        )
+    )
+
+
+@validate_call
+def image_align(
+    ref_img: str,
+    src_img: str,
+    out_img: str,
+    **opts,
+):
+    opts_str = arg_to_str(opts)
+
+    run(
+        "image_align {} {} {} -o {}".format(
+            opts_str,
+            ref_img,
+            src_img,
+            out_img,
+        )
+    )
+
+
+@validate_call
+def geodiff(
+    dem1: str,
+    dem2: str,
+    output_file_prefix: Optional[str],
+    **opts,
+):
+    out_str = arg_to_str(output_file_prefix)
+    if out_str:
+        out_str = "-o " + out_str
+    opts_str = arg_to_str(opts)
+
+    run(
+        "geodiff {} {} {} {}".format(
+            opts_str,
+            dem1,
+            dem2,
+            out_str,
+        )
+    )
+
+
+@validate_call
+def n_align(
+    cloud_files: List[str],
+    output_prefix: str,
+    **opts,
+):
+    opts_str = arg_to_str(opts)
+    cloud_str = arg_to_str(cloud_files)
+
+    run(
+        "n_align {} {} -o {}".format(
+            opts_str,
+            cloud_str,
+            output_prefix,
+        )
+    )
+
+
+@validate_call
+def corr_eval(
+    left_img: str,
+    right_img: str,
+    disparity_img: str,
+    output_prefix: str,
+    **opts,
+):
+    opts_str = arg_to_str(opts)
+
+    run(
+        "corr_eval {} {} {} {} {}".format(
+            opts_str,
+            left_img,
+            right_img,
+            disparity_img,
+            output_prefix,
+        )
+    )
